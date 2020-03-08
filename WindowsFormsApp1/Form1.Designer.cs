ca26d6615b3565f5f2d0632f264e9271b26d56bb
Pan hongming <qscdwe8199@gmail.com>
2020-03-08 11:11:03 +0800

    1. Login Work

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtDB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAML = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(139, 108);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(165, 25);
+            this.txtID.TabIndex = 1;
+            this.txtID.Text = "admin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Login Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(139, 146);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(165, 25);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Text = "innovator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Login DB:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "IP:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(139, 23);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(249, 25);
+            this.txtIP.TabIndex = 6;
+            this.txtIP.Text = "http://crm.gtocad.com:56666/espdm";
+            // 
+            // txtDB
+            // 
+            this.txtDB.Location = new System.Drawing.Point(139, 63);
+            this.txtDB.Name = "txtDB";
+            this.txtDB.Size = new System.Drawing.Size(249, 25);
+            this.txtDB.TabIndex = 7;
+            this.txtDB.Text = "Pacific Cycles20190604";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtAML
+            // 
+            this.txtAML.Location = new System.Drawing.Point(427, 44);
+            this.txtAML.Multiline = true;
+            this.txtAML.Name = "txtAML";
+            this.txtAML.Size = new System.Drawing.Size(361, 394);
+            this.txtAML.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(424, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "AML";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAML);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDB);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Checkin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtDB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAML;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aras.IOM;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,36 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        public Innovator inn { set; get; }
+        public string message { set; get; }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpServerConnection cnx = IomFactory.CreateHttpServerConnection(txtIP.Text,txtDB.Text,txtID.Text,txtPassword.Text);
+                Item login_result = cnx.Login();
+                if (!login_result.isError())
+                {
+                    inn = IomFactory.CreateInnovator(cnx);
+                    txtAML.Text = "Login success";
+                    return;
+                }
+                message = login_result.getErrorString();
+                txtAML.Text = message;
+            }
+            catch (Exception ex)
+            {
+                message = ex.ToString();
+                txtAML.Text = message;
+            }
         }
     }
 }
